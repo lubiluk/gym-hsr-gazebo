7fab3709446fdb322c6e3b3a03f86fda636449ce
Paweł Gajewski <lubiluk@gmail.com>
2020-11-02 15:30:20 +0100

    non related files moved to cembra

--- example.py
+++ example.py
@@ -1,15 +1,7 @@
 import gym
 import gym_hsr_gazebo
-import sys
-import signal
 
 env = gym.make('HsrbPush-v0')
-
-def signal_handler(sig, frame):
-    print('You pressed Ctrl+C!')
-    env.close
-    sys.exit(0)
-signal.signal(signal.SIGINT, signal_handler)
 
 env.reset()
 
